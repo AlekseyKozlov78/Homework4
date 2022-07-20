0b59838b2938f3b0b02f5b5298e6c0ce4527bd19
Aleksey <kozlovav78@mail.ru>
2022-07-20 22:02:49 +0300

    Решение задачи 27

---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27
+// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+
+int getSumNum(int num)
+{
+    int sum = 0;
+    int numLocal = 0;
+    while (num > 0)
+    {
+        numLocal = num % 10;
+        sum = sum + numLocal;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(getSumNum(num));
+
